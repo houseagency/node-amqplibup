3b28cca8b22133e0c5bf6473d6e5e4dff72248ab
alfred <alfred@nurd.nu>
2017-03-18 14:59:10 +0000

    removed dependency on amqplib-auto-recovery

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ amqplibup is a wrapper for [amqplib](https://www.npmjs.com/package/amqplib), pro
 		// conn is a connected amqplib connection object!
 
 		// Now, you can do your usual amqplib stuff.
-		// Please, not, this will run on every successful reconnect!
+		// Please, note, this will run on every successful reconnect!
 
 		con.createChannel((err, ch) => {
 			ch.assertQueue('my-queue');
@@ -21,12 +21,4 @@ amqplibup is a wrapper for [amqplib](https://www.npmjs.com/package/amqplib), pro
 * The callback will be called on every successful connect/reconnect.
 * There is no error argument on the callback, since no errors will ever be
   sent to the callback!
-
-## `amqplib-auto-recovery`
-
-The [amqplib-auto-recovery](https://www.npmjs.com/package/amqplib-auto-recovery)
-module is used for auto recovery when a connection is dropped. However, it does
-not support the scenario when your RabbitMQ/AMQP server is down when you are
-connecting the first time. That is why we wrapped both amqplib and
-amqplib-auto-recovery in this module.
 
--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const amqp = require('amqplib/callback_api');
-const autoRecovery = require('amqplib-auto-recovery');
 
 module.exports = (url, callback) => {
-	let wait = 200;
+	const startWait = 200;
 	const maxWait = 5000;
 	const waitIncrease = 400;
+	let wait = startWait;
 
 	function makeConn() {
-		try {
-			autoRecovery(amqp, {
-				onError: err => { /* console.error('AMQP connection error:', err); */ },
-				isErrorUnrecoverable: err => false
-			}).connect(url, (err, conn) => {
-				if (err) {
-					// console.error('Failed to connect:', err.message);
-					setTimeout(makeConn, wait);
-					if (wait != maxWait) wait += waitIncrease;
-					if (wait > maxWait) wait = maxWait;
-					return
-				}
 
-				if (typeof callback === 'function') callback(conn);
-			});
-		} catch (err) {
-			// console.error('Failed to connect:', err.message);
+		let retry = () => {
+
+			retry = () => {}; // Only actually retry once.
+
 			setTimeout(makeConn, wait);
 			if (wait != maxWait) wait += waitIncrease;
 			if (wait > maxWait) wait = maxWait;
+		};
+
+		try {
+			amqp.connect(url, (err, conn) => {
+				if (err) return retry();
+				conn.on('error', retry);
+				conn.on('close', retry);
+				if (typeof callback === 'function') {
+					wait = startWait;
+					callback(conn);
+				}
+			});
+		} catch (err) {
+			retry();
 		}
 	}
 	makeConn();
